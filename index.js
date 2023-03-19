93589077622f51c1e1957701cfda702d075074af
tharuka-pavith <tharukapavith2@gmail.com>
2023-03-19 09:29:25 +0530

    Add cors pakage

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const mongoose = require('mongoose');
 //Import routes file
 const routes = require('./routes/routes');
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:false,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+
 /** Connecting to database */
 require('dotenv').config();
 const mongoString = process.env.DATABASE_URL;
@@ -11,17 +19,19 @@ mongoose.connect(mongoString);
 const database = mongoose.connection
 database.on('error', (error) => {
     console.log(error)
-})
+});
 
 database.once('connected', () => {
     console.log('Database Connected');
-})
+});
 /************************* */
 
 const app = express(); //transfer the contents of Express into a new constant called app.
 
 app.use(express.json());
+app.use(cors()); //cors policy
 app.use('/api', routes); //Use routes file
+
 
 /**Middlewear to log requests */
 function logMiddlewear(req, res, next){
