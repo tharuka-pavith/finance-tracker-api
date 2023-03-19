f9d80e932e96c114f16d648dccc498eb32baa794
tharuka-pavith <tharukapavith2@gmail.com>
2023-03-19 09:31:08 +0530

    Add book_id attribute

--- models/entryModel.js
+++ models/entryModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const entrySchema = new mongoose.Schema({
+    "book_id": {
+        require: true,
+        type: String
+    },
     "date": {
         require: true,
         type: Date
@@ -14,8 +18,8 @@ const entrySchema = new mongoose.Schema({
     },
     "type": {
         require: true,
-        type: Boolean
-    },
+        type: Boolean // true for cash in
+    }
 });
 
 module.exports = mongoose.model('Entry', entrySchema);
