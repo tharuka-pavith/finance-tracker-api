436d867bbdc2aa16d231a00f085e427b917b1db4
tharuka-pavith <tharukapavith2@gmail.com>
2023-03-19 09:30:41 +0530

    Add new attributes to model

--- models/bookModel.js
+++ models/bookModel.js
@@ -8,7 +8,13 @@ const bookSchema = new mongoose.Schema({
     "user_id": {
         require: true,
         type: String
+    },
+    "total_in":{
+        type: Number
+    },
+    "total_out":{
+        type: Number
     }
 });
 
-module.exports = mongoose.model('Book', bookSchema)
+module.exports = mongoose.model('Book', bookSchema);
